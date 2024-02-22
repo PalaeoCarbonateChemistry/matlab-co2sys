cc8c32c47dc63291119dc76de720dd74df6e9376
rossidae <rossidae@protonmail.com>
2024-02-22 18:21:07 +0000

    Added test for static vs bound equilibrium constant generation

--- tests/EquilibriumConstantsTests.m
+++ tests/EquilibriumConstantsTests.m
@@ -102,22 +102,39 @@ classdef EquilibriumConstantsTests < matlab.unittest.TestCase
              salinity = inputs.salinity;
 
              pH_scale = inputs.pH_scale_in;
+
+             silicate = inputs.silicate;
+             phosphate = inputs.phosphate;
+             ammonia = inputs.ammonia;
+             sulphide = inputs.sulphide;
                 
              which_k1_k2 = inputs.which_k1k2;
              which_kso4 = inputs.which_kso4;
              which_kf = inputs.which_kf;
-             co2_correction = inputs.co2_pressure_correction;
+             which_ks = WhichKs(which_k1_k2,which_kso4,which_kf);
+             which_boron = inputs.which_boron;
+             co2_pressure_correction = inputs.co2_pressure_correction;
 
-             gas_constant = 83.14462618; % ml bar-1 K-1 mol-1,
-             sulphate_concentration = calculate_sulphate_concentration(salinity);
-             fluorine_concentration = calculate_fluorine_concentration(salinity);
+             composition = Composition(salinity)...
+                            .set_silicate_concentration(silicate/1e6)...
+                            .set_phosphate_concentration(phosphate/1e6)...
+                            .set_ammonia_concentration(ammonia/1e6)...
+                            .set_sulphide_concentration(sulphide/1e6)...
+                            .estimate_all_from_salinity(which_boron)...
+                            .remove_freshwater_species(which_ks)...
+                            .adjust_geosecs_species(which_ks)...
+                            .calculate_peng_correction(which_ks);
 
-             equilibrium_constants = EquilibriumConstants(temp_c,pressure_bar,salinity,pH_scale,which_k1_k2,which_kso4,which_kf);
-             calculated = equilibrium_constants.calculate();
+
+             % calculated = EquilibriumConstantsStatic.calculate_all(temp_c,pressure_bar,pH_scale,co2_correction,composition,which_ks);
+
+             calculated = EquilibriumConstants(temp_c,pressure_bar,composition,pH_scale,which_ks,co2_pressure_correction)...
+                              .calculate_all()...
+                              .as_map();
          end
      end
      methods (Test)
-         function test_calculate_all_vs_calculate_individual(testCase)
+         function test_calculate_all_vs_calculate_individual_static(testCase)
             [co2sys_inputs,~] = RandomTests.load_temperature_salinity_pressure_reference();
             all = EquilibriumConstantsTests.calculate_all_static(co2sys_inputs);
             individual = EquilibriumConstantsTests.calculate_individual_static(co2sys_inputs);
@@ -125,6 +142,15 @@ classdef EquilibriumConstantsTests < matlab.unittest.TestCase
             for key = all.keys()
                 testCase.verifyEqual(all(key{1}),individual(key{1}),"RelTol",1e-9);
             end
-        end
+         end
+         function test_static_vs_bound(testCase)
+            [co2sys_inputs,~] = RandomTests.load_temperature_salinity_pressure_reference();
+            static = EquilibriumConstantsTests.calculate_all_static(co2sys_inputs);
+            bound = EquilibriumConstantsTests.calculate_all_bound(co2sys_inputs);
+
+            for key = static.keys()
+                testCase.verifyEqual(static(key{1}),bound(key{1}),"RelTol",1e-9);
+            end
+         end
      end
 end
