64a6407a24d07e32b3ef5637df406f173f026263
rossidae <rossidae@protonmail.com>
2024-12-11 15:50:56 +0000

    Moved tests into a separate repo, included into this one as a submodule

--- tests/run_performance_tests.m
+++
@@ -1,8 +0,0 @@
-function result = run_performance_tests()
-    import matlab.unittest.TestSuite
-
-    performace_tests = TestSuite.fromClass(?PerformanceTests);
-    
-    fullSuite = performace_tests;
-    result = run(fullSuite);
-end
